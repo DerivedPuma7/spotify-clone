91e405fd3510543f3bd4aefb0ad0532002b89f84
Gustavo Figueiredo <gustavofigueiredo@deltaglobal.com.br>
2022-04-10 18:06:29 -0300

    my first deploy

--- server/index.js
+++ server/index.js
@@ -6,3 +6,9 @@ server.listen(config.port)
 .on('listening', () => { 
     logger.info(`rodandooo na porta ${config.port}`);
 });
+
+//impede que a aplicação caio caso um erro nao tratado aconteça
+//throw
+process.on('uncaughtException', (error) => logger.error(`unhandledRejection happened: ${error.stack || error}`));
+//promises
+process.on('unhandledRejection', (error) => logger.error(`unhandledRejection happened: ${error.stack || error}`));
